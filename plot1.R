75afac62df3cc78a8944a56c11a31e090d3ae3f7
Jeff Fletcher <jefffletcher@users.noreply.github.com>
2015-02-05 12:09:59 -0700

    .

--- plot1.R
+++ plot1.R
@@ -3,8 +3,14 @@ library(data.table)
 raw_data <- fread("../household_power_consumption.txt", header=TRUE)
 plot_data <- raw_data["Date" %in% c("1/2/2007","2/2/2007")]
 
-plot_data$Global_active_power <- as.numeric(plot_data$Global_active_power)
+plot_data <- within(plot_data, {
+  Global_active_power <- as.numeric(Global_active_power)
+})
 
 png("plot1.png", width=480, height=480, units="px")
-hist(plot_data$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+
+with(plot_data, {
+  hist(Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+})
+
 dev.off()
--- plot3.R
+++ plot3.R
@@ -3,14 +3,20 @@ library(data.table)
 raw_data <- fread("../household_power_consumption.txt", header=TRUE)
 plot_data <- raw_data["Date" %in% c("1/2/2007","2/2/2007")]
 
-plot_data$my_date <- as.POSIXct(strptime(paste(plot_data$Date, plot_data$Time, sep=" "), format="%d/%m/%Y %H:%M:%S"))
-plot_data$Sub_metering_1 <- as.numeric(plot_data$Sub_metering_1)
-plot_data$Sub_metering_2 <- as.numeric(plot_data$Sub_metering_2)
-plot_data$Sub_metering_3 <- as.numeric(plot_data$Sub_metering_3)
+plot_data <- within(plot_data, {
+  my_date <- as.POSIXct(strptime(paste(Date, Time, sep=" "), format="%d/%m/%Y %H:%M:%S"))
+  Sub_metering_1 <- as.numeric(Sub_metering_1)
+  Sub_metering_2 <- as.numeric(Sub_metering_2)
+  Sub_metering_3 <- as.numeric(Sub_metering_3)
+})
 
 png("plot3.png", width=480, height=480, units="px")
-plot(plot_data$my_date, plot_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-lines(plot_data$my_date, plot_data$Sub_metering_2, col="red")
-lines(plot_data$my_date, plot_data$Sub_metering_3, col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+
+with(plot_data, {
+  plot(my_date, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(my_date, Sub_metering_2, col="red")
+  lines(my_date, Sub_metering_3, col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+})
+
 dev.off()
