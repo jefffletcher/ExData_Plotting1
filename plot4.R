a165fda0f1884d8b5c10e127c387b5874b10574c
Jeff Fletcher <jefffletcher@users.noreply.github.com>
2015-02-05 12:16:56 -0700

    .

--- plot4.R
+++ plot4.R
@@ -20,14 +20,14 @@ par(mfrow=c(2,2))
 with(plot_data, {
   plot(my_date, Global_active_power, type="l", xlab="", ylab="Global Active Power")
 
-  plot(my_date, Voltage, type="l", xlab="datetime", ylab="Voltage")
+  plot(my_date, Voltage, type="l", xlab="datetime")
 
   plot(my_date, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   lines(my_date, Sub_metering_2, col="red")
   lines(my_date, Sub_metering_3, col="blue")
   legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, bty="n")
 
-  plot(my_date, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+  plot(my_date, Global_reactive_power, type="l", xlab="datetime")
 })
 
 dev.off()
